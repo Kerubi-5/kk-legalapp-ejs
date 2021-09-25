f2c71bead0d680161f5dd3eb1e1b752669678fbf
John Kim Querobines <querobinesj@gmail.com>
2021-09-25 11:42:22 +0800

    fixed complaint

--- routes/complaints.js
+++ routes/complaints.js
@@ -10,6 +10,7 @@ const Complaint = require('../models/Complaint')
 const isClient = require('./auth').isClient
 const isLawyer = require('./auth').isLawyer
 const isAdmin = require('./auth').isAdmin;
+const { ObjectId } = require('bson');
 
 // This is how to populate subfields
 // router.get('/', (req, res) => {
@@ -19,5 +20,69 @@ const isAdmin = require('./auth').isAdmin;
 //         res.send(result)
 //     })
 // })
+
+// COMPLAINT POST CONFIRMATION VIEW
+router.post('/consultation', async (req, res) => {
+    //    const client_id = ObjectId(req.user._id)
+    const client_id = ObjectId(req.user._id)
+    const details = req.body
+
+    res.render('consultation-confirmation', {
+        user_id: client_id, details
+    })
+})
+
+// COMPLAINT POST SUBMIT
+router.post('/consultation/submit', (req, res) => {
+    const client_id = ObjectId(req.user._id)
+
+    const {
+        parent_name,
+        parent_address,
+        service_id,
+        files_and_attachments,
+        lawyer_id,
+    } = req.body
+
+    let errors = []
+
+    if (!service_id || !files_and_attachments || lawyer_id) {
+        errors.push('Please fill all the required fields')
+    }
+
+    if (errors.length > 0) {
+        res.render('dashboard', {
+            errors
+        })
+    } else {
+        let newComplaint = new Complaint
+        // If minor or not
+        if (!parent_name) {
+            newComplaint = new Complaint({
+                service_id,
+                files_and_attachments,
+                lawyer_id,
+            })
+        } else {
+            newComplaint = new Complaint({
+                parent_name,
+                parent_address,
+                service_id,
+                files_and_attachments,
+                lawyer_id,
+            })
+        }
+
+        newComplaint.save()
+        req.flash('sucess_msg', 'Complaint Successfully Submitted')
+        res.redirect('/dashboard')
+    }
+})
+
+
+// TRANSACTION VIEW
+router.get('/:id', (req, res) => {
+
+})
 
 module.exports = router
