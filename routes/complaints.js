1e28506bfdc9e71c4893ef4c0f93c311d7b88280
John Kim Querobines <querobinesj@gmail.com>
2021-09-29 17:01:31 +0800

    complaint transaction lawyer side done

--- routes/complaints.js
+++ routes/complaints.js
@@ -13,7 +13,7 @@ const isAdmin = require('./auth').isAdmin;
 const isAuth = require('./auth').isAuth
 const { ObjectId } = require('bson');
 
-// COMPLAINT VIEW
+// COMPLAINT VIEW TRANSACTION ONGOING
 router.get('/complaints/:id', isAuth, (req, res) => {
     const user_id = ObjectId(req.user._id)
     const complaint_id = ObjectId(req.params.id)
@@ -27,7 +27,7 @@ router.get('/complaints/:id', isAuth, (req, res) => {
 
         // Only users involved in this complaint will be able to see the content of the complaint
         if (user_id.equals(result.client_id._id) || user_id.equals(result.lawyer_id._id))
-            res.render('consultation-view', { user_id: user_id, result, user_type: user_type })
+            res.render('consultation-view', { user_id: user_id, result, user_type: user_type, a_type: "ongoing" })
         else
             res.status(401).send("You do not have the authority to view this resource")
     })
@@ -93,9 +93,24 @@ router.post('/consultation', isClient, (req, res) => {
     res.redirect('/dashboard')
 })
 
-// TRANSACTION VIEW
-router.get('/:id', (req, res) => {
+// COMPLAINT PENDING VIEW LAWYER SIDE
+router.get('/complaints/pending/:id', isLawyer, (req, res) => {
+    const user_id = ObjectId(req.user._id)
+    const complaint_id = ObjectId(req.params.id)
 
+    Complaint.findOne({ _id: complaint_id }).populate("client_id").populate("lawyer_id").exec(async (err, result) => {
+        if (err) throw err
+
+        let user_doc = await User.findOne({ _id: user_id })
+
+        const user_type = user_doc.user_type
+
+        // Only users involved in this complaint will be able to see the content of the complaint
+        if (user_id.equals(result.client_id._id) || user_id.equals(result.lawyer_id._id))
+            res.render('consultation-view', { user_id: user_id, result, user_type: user_type, a_type: "pending" })
+        else
+            res.status(401).send("You do not have the authority to view this resource")
+    })
 })
 
 router.get('/', (req, res) => {
