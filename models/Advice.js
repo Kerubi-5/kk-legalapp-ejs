4cc466563b5cfb8cfa187eea24937ce34993ccea
John Kim Querobines <querobinesj@gmail.com>
2021-10-16 17:07:10 +0800

    formatting fixed

--- models/Advice.js
+++ models/Advice.js
@@ -1,41 +1,39 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const AdviceSchema = new mongoose.Schema({
-    client_id: {
+  client_id: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: "User",
+  },
+  date_submitted: {
+    type: Date,
+    default: Date.now(),
+  },
+  legal_title: {
+    type: String,
+  },
+  legal_description: {
+    type: String,
+  },
+  is_resolved: {
+    type: Boolean,
+    default: false,
+  },
+  lawyers: [
+    {
+      _id: {
         type: mongoose.SchemaTypes.ObjectId,
-        ref: "User"
-    },
-    date_submitted: {
+        ref: "User",
+      },
+      answer: {
+        type: String,
+      },
+      date_submitted: {
         type: Date,
-        default: Date.now()
+        default: Date.now(),
+      },
     },
-    legal_title: {
-        type: String
-    },
-    legal_description: {
-        type: String
-    },
-    is_resolved: {
-        type: Boolean,
-        default: false
-    },
-    lawyers: [{
-        _id: {
-            type: mongoose.SchemaTypes.ObjectId,
-            ref: "User"
-        },
-        answer: {
-            type: String
-        },
-        is_resolved: {
-            type: Boolean,
-            default: false
-        },
-        date_submitted: {
-            type: Date,
-            default: Date.now()
-        }
-    }]
-})
+  ],
+});
 
-module.exports = mongoose.model('Advice', AdviceSchema)
+module.exports = mongoose.model("Advice", AdviceSchema);
