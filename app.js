a3e66ceb7bea968861bbf755e2f79d6eff8d91fb
John Kim Querobines <querobinesj@gmail.com>
2021-11-08 13:48:54 +0800

    added console log and removed unused ejs files

--- app.js
+++ app.js
@@ -122,6 +122,7 @@ app.use((req, res, next) => {
 
 // HANDLE ALL ERRORS
 app.use((err, req, res, next) => {
+  console.log(err)
   res.status(err.status || 500)
   res.render("../views/pages/error", { layout: false, err })
 })
