0e604609d62777864fa4a99a226354ede4325c9c
John Kim Querobines <querobinesj@gmail.com>
2021-11-22 10:38:18 +0800

    defers javascript and jquery files

--- models/Solution.js
+++ models/Solution.js
@@ -1,37 +1,37 @@
-const mongoose = require('mongoose')
-const { customAlphabet } = require('nanoid');
-const alphabet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+const mongoose = require("mongoose");
+const { customAlphabet } = require("nanoid");
+const alphabet =
+  "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 const nanoid = customAlphabet(alphabet, 12);
 
-
 const SolutionSchema = new mongoose.Schema({
-    _id: {
-        type: String,
-        default: () => nanoid()
-    },
-    complaint_id: {
-        type: String,
-        ref: "Complaint"
-    },
-    video_link: {
-        type: String
-    },
-    summary: {
-        type: String
-    },
-    recommendations: {
-        type: String
-    },
-    date_completed: {
-        type: Date,
-        default: Date.now()
-    },
-    re_appointment: {
-        type: Date
-    },
-    supporting_document: {
-        type: String
-    }
-})
+  _id: {
+    type: String,
+    default: () => nanoid(),
+  },
+  complaint_id: {
+    type: String,
+    ref: "Complaint",
+  },
+  video_link: {
+    type: String,
+  },
+  summary: {
+    type: String,
+  },
+  recommendations: {
+    type: String,
+  },
+  date_completed: {
+    type: Date,
+    default: Date.now(),
+  },
+  re_appointment: {
+    type: Date,
+  },
+  supporting_document: {
+    type: String,
+  },
+});
 
-module.exports = mongoose.model('Solution', SolutionSchema)
+module.exports = mongoose.model("Solution", SolutionSchema);
