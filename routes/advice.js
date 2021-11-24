ae6da80b7ca12b1e20226e8e95d136fd6b4eeb5d
John Kim Querobines <querobinesj@gmail.com>
2021-11-24 12:44:58 +0800

    adds adviceController

--- routes/advice.js
+++ routes/advice.js
@@ -1,133 +1,23 @@
 const express = require("express");
 const router = express.Router();
 
-// Load User model
-const User = require("../models/User");
-const Notification = require("../models/Notification");
-const Advice = require("../models/Advice");
+// Controller
+const adviceController = require("../controllers/adviceController");
 
 // Auth types
 const isLawyer = require("./auth").isLawyer;
 const isAuth = require("./auth").isAuth;
 
-router.get("/", isAuth, async (req, res, next) => {
-  try {
-    const id = req.user._id;
+router.route("/", isAuth)
+  .get(adviceController.index)
+  .post(adviceController.submitPost)
 
-    // DATA VARIABLE QUERIES
-    const filter = req.query.filter;
-    let advicesDoc;
+router.get("/main", isAuth, adviceController.adminPost);
 
-    const notifications = await Notification.find({ target: id });
+router.get("/:id", isAuth, adviceController.findPostById);
 
-    // APPLY SEARCH LOGIC HERE
-    if (filter) {
-      advicesDoc = await Advice.find({
-        legal_title: new RegExp(filter, "i"),
-      }).sort({ date_submitted: "desc" });
-    } else {
-      advicesDoc = await Advice.find({}).sort({ date_submitted: "desc" });
-    }
+router.patch("/comment/:id", isLawyer, adviceController.postComment);
 
-    res.render("./advice/index", {
-      currentUser: req.user,
-      notifications,
-      advices: advicesDoc,
-    });
-  } catch (err) {
-    next(err);
-  }
-});
-
-router.get("/main", isAuth, async (req, res, next) => {
-  const user_id = req.user._id;
-  const notifications = await Notification.find({ target: user_id });
-
-  res.render("./advice/advice-sample", {
-    currentUser: req.user,
-    notifications,
-  });
-});
-
-router.post("/", isAuth, async (req, res, next) => {
-  try {
-    const id = req.user._id;
-    const { legal_title, legal_description } = req.body;
-
-    const newAdvice = new Advice({
-      client_id: id,
-      legal_title: legal_title,
-      legal_description: legal_description,
-    });
-
-    await newAdvice.save();
-
-    res.redirect("/advice");
-  } catch (err) {
-    next(err);
-  }
-});
-
-router.get("/:id", isAuth, async (req, res, next) => {
-  try {
-    const id = req.user._id;
-    const advice_id = req.params.id;
-
-    const notifications = await Notification.find({ target: id });
-    const adviceDoc = await Advice.findOne({ _id: advice_id }).populate(
-      "lawyers._id"
-    );
-    const userDoc = await User.findOne({ _id: adviceDoc.client_id });
-    const lawyersDoc = adviceDoc.lawyers;
-    const loginDoc = await User.findOne({ _id: id });
-
-    if (typeof adviceDocs == undefined) adviceDocs = null;
-
-    res.render("./advice/advice-view", {
-      currentUser: req.user,
-      notifications,
-      adviceDoc,
-      userDoc,
-      lawyersDoc,
-      loginDoc,
-    });
-  } catch (err) {
-    next(err);
-  }
-});
-
-router.patch("/comment/:id", isLawyer, async (req, res, next) => {
-  try {
-    const id = req.user._id;
-    const advice_id = req.params.id;
-    const { answer } = req.body;
-
-    const lawyer = {
-      _id: id,
-      answer: answer,
-    };
-
-    const adviceDoc = await Advice.findByIdAndUpdate(
-      { _id: advice_id },
-      { $push: { lawyers: lawyer } }
-    );
-
-    res.redirect("/advice/" + advice_id);
-  } catch (err) {
-    next(err);
-  }
-});
-
-router.get("/vote/:id", isAuth, async (req, res, next) => {
-  try {
-    const vote_id = req.params.id;
-
-    await Advice.findByIdAndUpdate({ _id: vote_id }, { is_resolved: true });
-
-    res.redirect("/advice/" + vote_id);
-  } catch (err) {
-    next(err);
-  }
-});
+router.get("/vote/:id", isAuth, adviceController.markPostResolved);
 
 module.exports = router;
